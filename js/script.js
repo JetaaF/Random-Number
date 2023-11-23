5d6250baa9ff7db0c2d924ccd92fd50cd4918bc2
deondaci <deondaci1@gmail.com>
2023-11-23 07:26:11 -0800

    publikimi ne github, dtr random number

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let number  = document.querySelector('.number')
+let result = document.querySelector('.result')
+let randomNr;
+
+function CheckNumber() {
+    randomNr = Math.floor(Math.random() * 100)
+    number.innerHTML = randomNr;
+    if (number.textContent >= 10) {
+        result.textContent = "Success"
+        result.classList.add("success")
+        result.classList.remove("error")
+    }
+    else if (number.textContent < 10) {
+        result.textContent = "Fail, please try again!"
+        result.classList.add("error")
+        result.classList.remove("success")
+    }
+    
+
+}
